d7fdf62579db842480e989ebdd9d26e00bef6599
Unknown <kevinwtenweerde@gmail.com>
2018-05-09 14:53:26 +0200

    Gradle project commit

---
+++ boodschappen/src/main/java/com/kevinw/nl/boodschappen/BoodschappenApplication.java
@@ -0,0 +1,12 @@
+package com.kevinw.nl.boodschappen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoodschappenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoodschappenApplication.class, args);
+	}
+}
---
+++ boodschappen/src/test/java/com/kevinw/nl/boodschappen/BoodschappenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kevinw.nl.boodschappen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoodschappenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
